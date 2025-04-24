a85d036de2b75d60e386937356137a8bf9329820
Drishita Paul <146695863+drishitapaul@users.noreply.github.com>
2025-04-25 03:17:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,28 +11,51 @@ const changeTurn = ()=>{
 }
 
 // Function to check for a win
-const checkWin = ()=>{
+const checkWin = () => {
     let boxtext = document.getElementsByClassName('boxtext');
-    let wins = [
-        [0, 1, 2, 5, 5, 0],
-        [3, 4, 5, 5, 15, 0],
-        [6, 7, 8, 5, 25, 0],
-        [0, 3, 6, -5, 15, 90],
-        [1, 4, 7, 5, 15, 90],
-        [2, 5, 8, 15, 15, 90],
-        [0, 4, 8, 5, 15, 45],
-        [2, 4, 6, 5, 15, 135],
-    ]
-    wins.forEach(e =>{
-        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "") ){
-            document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won"
-            isgameover = true
-            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
-            document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
-            document.querySelector(".line").style.width = "20vw";
+    let boxes = document.getElementsByClassName('box');
+
+    const wins = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    wins.forEach((e) => {
+        if (
+            boxtext[e[0]].innerText === boxtext[e[1]].innerText &&
+            boxtext[e[2]].innerText === boxtext[e[1]].innerText &&
+            boxtext[e[0]].innerText !== ""
+        ) {
+            document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won";
+            isgameover = true;
+            document.querySelector('.imgbox img').style.width = "200px";
+
+            // Dynamically calculate line position
+            let box1 = boxes[e[0]].getBoundingClientRect();
+            let box2 = boxes[e[2]].getBoundingClientRect();
+
+            let container = document.querySelector('.container').getBoundingClientRect();
+
+            let x1 = box1.left + box1.width / 2 - container.left;
+            let y1 = box1.top + box1.height / 2 - container.top;
+            let x2 = box2.left + box2.width / 2 - container.left;
+            let y2 = box2.top + box2.height / 2 - container.top;
+
+            let length = Math.hypot(x2 - x1, y2 - y1);
+            let angle = Math.atan2(y2 - y1, x2 - x1) * (180 / Math.PI);
+
+            const line = document.querySelector('.line');
+            line.style.width = `${length}px`;
+            line.style.transform = `translate(${x1}px, ${y1}px) rotate(${angle}deg)`;
         }
-    })
-}
+    });
+};
 
 // Game Logic
 // music.play()
